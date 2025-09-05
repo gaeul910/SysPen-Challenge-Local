c67d776de6431bbaa76cb5f2874f274580d4be14
gaeul910 <chw910@chungbuk.ac.kr>
2025-09-05 22:27:45 +0900

    Added the challenge temp_challenge

---
+++ Challenges/temp_challenge/challenge.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int afunc()
+{
+    printf("Noooo");
+    return 0;
+}
+
+int flag()
+{
+    printf("You Succeed, {Flag}");
+
+    return 0;
+}
+
+int main()
+{
+    afunc();
+    return 0;
+}
