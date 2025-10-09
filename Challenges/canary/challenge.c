dd69cb316a4b12c8baffac167f8f8c68cf1c3c12
gaeul910 <chw910@chungbuk.ac.kr>
2025-10-09 22:36:29 +0900

    Challenge canary update

--- Challenges/canary/challenge.c
+++ Challenges/canary/challenge.c
@@ -5,11 +5,18 @@ int vul()
     char username[0x10];
     char user_id[0x10];
     printf("Enter Username: ");
-    gets(username);
+    read(0, username, 0x100);
     printf("Enter User ID: ");
-    gets(user_id);
+    read(0, user_id, 0x100);
     return 0;
 }
+
+int get_flag()
+{
+    printf("This is Flag!");
+    return 0;
+}
+
 int main()
 {
     vul();
